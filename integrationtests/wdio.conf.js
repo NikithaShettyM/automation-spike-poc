82cad3cd76fff9bb1105cb7d98b82a414a922da9
Niketha_Mangalore <nikman12@in.ibm.com>
2021-09-06 20:20:58 +0530

    change reporters format

--- integrationtests/wdio.conf.js
+++ integrationtests/wdio.conf.js
@@ -154,15 +154,18 @@ exports.config = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: https://webdriver.io/docs/dot-reporter.html
-  reporters: ['spec', 'allure'],
-    reporterOptions: {
-        allure: {
-            outputDir: 'allure-results',
-            disableWebdriverStepsReporting: true,
-            disableWebdriverScreenshotsReporting: true,
-            useCucumberStepReporter: true
-        }
-    },
+  reporters: [
+    'spec', 
+    [
+      'allure',
+      {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: true,
+        useCucumberStepReporter: true
+      }
+    ]  
+  ],
   //
   // If you are using Cucumber you need to specify the location of your step definitions.
   cucumberOpts: {
