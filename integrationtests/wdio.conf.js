e26476d570636744c68c1c7ea72e47b7e55d624e
Niketha_Mangalore <nikman12@in.ibm.com>
2021-09-07 12:38:58 +0530

    set backtrace: true

--- integrationtests/wdio.conf.js
+++ integrationtests/wdio.conf.js
@@ -154,23 +154,17 @@ exports.config = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: https://webdriver.io/docs/dot-reporter.html
-  reporters: [
-    'spec', 
-    [
-      'allure',
-      {
-        outputDir: 'allure-results',
-        disableWebdriverStepsReporting: true,
-        disableWebdriverScreenshotsReporting: true,
-        useCucumberStepReporter: true
-      }
-    ]  
-  ],
+  reporters: ['spec', 'allure'],
+    reporterOptions: {
+        allure: {
+            outputDir: 'allure-results',
+        },
+    },
   //
   // If you are using Cucumber you need to specify the location of your step definitions.
   cucumberOpts: {
     // <boolean> show full backtrace for errors
-    backtrace: false,
+    backtrace: true,
     // <string[]> module used for processing required features
     requireModule: ['@babel/register'],
     // <boolean< Treat ambiguous definitions as errors
